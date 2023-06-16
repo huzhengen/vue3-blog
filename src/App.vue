4223ca4d173b77c8854fc55c905b0cf0a5807816
huzhengen <huzhengen@163.com>
2023-06-16 21:57:20 +0800

    添加路由

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,4 @@
 <script setup></script>
-
 <template>
-  <header class="no-login" id="header">
-    <h1>LET'S SHARE</h1>
-    <p>精品文章汇聚(如提交垃圾文章请勿展示在首页)</p>
-    <div class="btns">
-      <el-button>立即登录</el-button>
-      <el-button>注册账号</el-button>
-    </div>
-  </header>
+  <router-view />
 </template>
-
-<style scoped lang="scss">
-header {
-  background-color: #149739;
-  text-align: center;
-  color: #fff;
-  padding: 50px 0;
-  h1 {
-  }
-  p {
-    margin-top: 20px;
-  }
-  .btns {
-    margin-top: 20px;
-  }
-}
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="no-login" id="header">
+    <h1>LET'S SHARE</h1>
+    <p>精品文章汇聚(如提交垃圾文章请勿展示在首页)</p>
+    <div class="btns">
+      <router-link to="/login">
+        <el-button>立即登录</el-button>
+      </router-link>
+      <el-button>注册账号</el-button>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  background-color: #149739;
+  text-align: center;
+  color: #fff;
+  padding: 50px 0;
+  p {
+    margin-top: 20px;
+  }
+  .btns {
+    margin-top: 20px;
+    a:first-child {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
