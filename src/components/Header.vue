2c01f617e3ed056f4386b47c9633e957ad067f43
huzhengen <huzhengen@163.com>
2023-07-07 21:19:21 +0800

    博客详情页面 css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -75,6 +75,8 @@ header.no-login {
   padding: 50px 0;
   h1 {
     font-size: 46px;
+    margin: 0;
+    padding: 0;
   }
   p {
     margin-top: 30px;
@@ -98,6 +100,8 @@ header.login {
     align-items: center;
     justify-content: space-between;
     h1 {
+      margin: 0;
+      padding: 0;
       a {
         color: #fff;
       }
